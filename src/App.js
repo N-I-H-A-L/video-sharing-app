0906b0d9da729b259e18469510dde5b698832bac
Nihal <nihalkumar9749@gmail.com>
2023-11-03 18:57:10 +0530

    Menu Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+import { darkTheme, lightTheme } from './utils/Theme';
+import { useState } from 'react';
 
 function App() {
+  const [theme, setTheme] = useState("dark");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //ThemeProvider of styled components helps to set theme easily.
+    <ThemeProvider theme={theme=="dark"?darkTheme:lightTheme}>
+      <Container>
+        <Menu getTheme={theme} setTheme={setTheme}/>
+        <Main>
+          <Navbar />
+          <Wrapper>
+
+          </Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: #181818;
+`;
+
+const Wrapper = styled.div`
+
+`;
 
 export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,179 @@
+import React from 'react';
+import styled from "styled-components";
+import LogoImg from "../images/youtube-logo-png-2074.png";
+import { Link } from "react-router-dom";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+
+const Menu = ({ getTheme, setTheme }) => {
+  const toggleTheme = () =>{
+    if(getTheme=="dark") setTheme("light");
+    else setTheme("dark");
+  }
+  return (
+    <Container>
+      <Wrapper>
+
+        <Logo>
+          <Img src={LogoImg} alt="logo"/>
+          <Name>MeTube</Name>
+        </Logo>
+
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Hr />
+
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <LoginBtn><AccountCircleOutlinedIcon /> <SignIn>SIGN IN</SignIn></LoginBtn>
+        </Login>
+        <Hr />
+
+        <Hr />
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+
+        <Item>
+          <SettingsBrightnessOutlinedIcon />
+          <ToggleBtn onClick={toggleTheme}>{(getTheme=="dark")?<span>Light Mode</span>:<span>Dark Mode</span>}</ToggleBtn>
+        </Item>
+      </Wrapper>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  flex: 1;
+  background-color: ${({theme}) => theme.bg}; //using ThemeProvider
+  color: ${({theme}) => theme.text};
+  height: 100vh;
+  font-size: 14px;
+  padding: 0px 20px;
+  position: sticky;
+  top: 0;
+`;
+
+const Wrapper = styled.div`
+
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  height: 60px;
+`;
+
+const Name = styled.span`
+  position: relative;
+  left: -12%;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 6px;
+
+  &:hover{
+    cursor: pointer;
+  }
+`;
+
+const Hr = styled.hr`
+  border: 0.5px;
+  margin: 10px 0px;
+`;
+
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const LoginBtn = styled.button`
+  background-color: transparent;
+  padding: 7px 5px;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  font-weight: bold;
+  border-radius: 3px;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  width: 60%;
+`;
+
+const SignIn = styled.span`
+  position: relative;
+  left: 5%;
+`;
+
+const ToggleBtn = styled.div`
+  
+`;
+
+export default Menu
