d9e05f33bb0694d4dbc6d3bd9c3b18a7d32d2363
Nihal <nihalkumar9749@gmail.com>
2023-11-09 07:56:30 +0530

    Set up Axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Card from '../components/Card';
+import axiosClient from '../axios.js';
 
 const Home = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const fetchVideos = async ()=>{
+      await axiosClient.get("/video/random")
+        .then((res)=>{
+          //res.data will contain the information sent by the API (i.e., the videos).
+          console.log(res.data);
+          setVideos(res.data);
+        })
+        .catch((err)=>{
+          console.log(err);
+        });
+    }
+
+    fetchVideos();
+    //Created a function then called it since, the function of useEffect can't be declared as async.
+  }, []);
+
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {videos.map(()=>{
+        return <Card/>
+      })}
     </Container>
   )
 }
