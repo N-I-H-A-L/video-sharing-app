e379f0d6b3be1d4709565142bfb6d51514ddaca2
Nihal Baranwal <nihalkumar9749@gmail.com>
2024-06-09 20:49:10 +0530

    Updated Base URL for axios

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    // baseURL: process.env.REACT_APP_SERVER,
-    baseURL: `http://localhost:5000/api`,
+    baseURL: process.env.REACT_APP_SERVER,
+    // baseURL: `http://localhost:5000/api`,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
